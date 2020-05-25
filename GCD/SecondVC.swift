c8bf91c608853ae905d01f3b90ca99a1a61a8772
Aleksandr Reunov <reun_alex@mail.ru>
2020-05-25 20:55:30 +0300

    delay asyncAfter

--- GCD/SecondVC.swift
+++ GCD/SecondVC.swift
@@ -29,9 +29,49 @@ class SecondViewController: ViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         fetchImage()
+        delay(3) {
+            self.loginAlert()
+        }
     }
     
+//    метод  delay asyncAfter
+    
+    fileprivate func delay (_ delay: Int, closure: @escaping () -> ()) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(delay)) {
+            closure()
+        }
+    }
+    
+//    метод для создания логина alert
+    
+    fileprivate func loginAlert() {
+        let ac = UIAlertController(title: "Зарегистрированы?", message: "Введите Ваш логин и пароль", preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        let cancelAction = UIAlertAction(title: "Отмена", style: .default, handler: nil)
+        
+        ac.addAction(okAction)
+        ac.addAction(cancelAction)
+        
+        ac.addTextField { (usernameTF) in
+            usernameTF.placeholder = "Введите логин"
+        }
+        
+        ac.addTextField { (userPasswordTF) in
+            userPasswordTF.placeholder = "Введие пароль"
+            userPasswordTF.isSecureTextEntry = true
+            
+        }
+        
+        self.present(ac, animated: true, completion: nil)
+    }
+    
+    
+    
+    
+    
     fileprivate func fetchImage() {
+        
         imageURL = URL (string: "https://upload.wikimedia.org/wikipedia/commons/9/96/Apple_III%2B.jpg")
         activityIndicator.isHidden = false
         activityIndicator.startAnimating()
